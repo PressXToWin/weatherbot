c15b1a4fe7e680c8652c4227701165d700104427
PressXToWin <PressXToWin@users.noreply.github.com>
2023-09-15 14:35:35 +0700

    bot init

---
+++ bot.py
@@ -0,0 +1,40 @@
+from aiogram import Bot, Dispatcher, types, executor
+from settings import bot_config
+
+bot = Bot(token=bot_config.bot_token)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start_message(message: types.Message):
+    markup = types.reply_keyboard.ReplyKeyboardMarkup(row_width=2)
+    btn1 = types.KeyboardButton('Погода в моём городе')
+    btn2 = types.KeyboardButton('Погода в другом месте')
+    btn3 = types.KeyboardButton('История')
+    btn4 = types.KeyboardButton('Установить свой город')
+    markup.add(btn1, btn2, btn3, btn4)
+    text = f'Привет, {message.from_user.first_name}, я бот, который расскажет тебе о погоде на сегодня.'
+    await message.answer(text, reply_markup=markup)
+
+
+@dp.message_handler(regexp='Погода в моём городе')
+async def start_message(message: types.Message):
+    markup = types.reply_keyboard.ReplyKeyboardMarkup(row_width=2)
+    btn1 = types.KeyboardButton('Меню')
+    markup.add(btn1)
+    text = 'NotImplementedYet'
+    await message.answer(text, reply_markup=markup)
+
+
+@dp.message_handler(regexp='Меню')
+async def start_message(message: types.Message):
+    markup = types.reply_keyboard.ReplyKeyboardMarkup(row_width=2)
+    btn1 = types.KeyboardButton('Погода в моём городе')
+    btn2 = types.KeyboardButton('Погода в другом месте')
+    btn3 = types.KeyboardButton('История')
+    btn4 = types.KeyboardButton('Установить свой город')
+    markup.add(btn1, btn2, btn3, btn4)
+    text = f'Привет, {message.from_user.first_name}, я бот, который расскажет тебе о погоде на сегодня.'
+    await message.answer(text, reply_markup=markup)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
