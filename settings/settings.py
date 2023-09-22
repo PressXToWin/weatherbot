22f2559ed4f29028a0876c88fe63647019fe5f6e
PressXToWin <PressXToWin@users.noreply.github.com>
2023-09-22 11:45:50 +0700

    fix misplaced parameter

--- settings/settings.py
+++ settings/settings.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GEO_KEY = os.getenv('GEO_KEY')
-WEATHER_KEY = {'X-Yandex-API-Key': os.getenv('WEATHER_KEY', 'sqlite:////app/bot.db')}
+WEATHER_KEY = {'X-Yandex-API-Key': os.getenv('WEATHER_KEY')}
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:////app/bot.db')
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 TG_BOT_ADMIN = os.getenv('TG_BOT_ADMIN').split()
